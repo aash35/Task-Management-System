f5c57560830f379705487b306db34016939017e6
Adrian Ash <adrianash2705@gmail.com>
2019-11-26 08:31:27 +1000

    added password tool tip

--- js/js.js
+++ js/js.js
@@ -44,6 +44,9 @@ function toasthide() {
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, { coverTrigger: false });
+    
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
 
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
